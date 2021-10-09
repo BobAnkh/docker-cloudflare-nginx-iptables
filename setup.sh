175a0656cd4897feafc3cd04cca4e595db7bfe2b
BobAnkh <bobankhshen@gmail.com>
2021-10-09 15:31:01 +0000

    feat(setup): add env creation and template update

--- setup.sh
+++ setup.sh
@@ -4,12 +4,19 @@
  # @Github       : https://github.com/BobAnkh
  # @Date         : 2021-10-09 15:00:15
  # @LastEditors  : BobAnkh
- # @LastEditTime : 2021-10-09 15:26:12
+ # @LastEditTime : 2021-10-09 15:29:52
  # @Description  : 
  # Copyright 2021 BobAnkh
 ### 
 
 set -e
+
+echo "Copy .env file from sample"
+cp .env.sample .env
+
+echo "Update nginx.tmpl..."
+DEFAULT_NGINX_TEMPLATE_URL="https://raw.githubusercontent.com/nginx-proxy/nginx-proxy/master/nginx.tmpl"
+curl -L "$DEFAULT_NGINX_TEMPLATE_URL" -o nginx.tmpl
 
 echo "Generate necessary certificates..."
 mkdir -p ./data/certs && cd ./data/certs
@@ -21,6 +28,6 @@ rm default.csr
 openssl dhparam -out dhparam.pem 4096
 
 cd ../..
-echo "done!"
+echo "Done!"
 echo "Please add your cloudflare origin certificates mannully for each website"
 echo "in a format: example.com.crt and example.com.key"
